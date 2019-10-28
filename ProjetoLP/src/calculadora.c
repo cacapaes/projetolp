aa07a387300a024c9d2fea3ecf62f54a15c24d32
carlos <carlos@carlos-S550CA>
2019-10-28 09:31:14 -0300

    criação dos módulos para cálculo

---
+++ ProjetoLP/src/calculadora.c
@@ -0,0 +1,8 @@
+/*
+ * calculadora.c
+ *
+ *  Created on: 28 de out de 2019
+ *      Author: carlos
+ */
+
+
---
+++ ProjetoLP/src/calculadora.h
@@ -0,0 +1,13 @@
+/*
+ * calculadora.h
+ *
+ *  Created on: 28 de out de 2019
+ *      Author: carlos
+ */
+
+#ifndef CALCULADORA_H_
+#define CALCULADORA_H_
+
+
+
+#endif /* CALCULADORA_H_ */
