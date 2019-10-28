aaebfc743a46c9554ec63b727a4e5e6eba8269f7
carlos <carlos@carlos-S550CA>
2019-10-28 15:20:02 -0300

    Implementação dos requisitos de arquivo

---
+++ ProjetoLP/src/arquivo.c
@@ -0,0 +1,10 @@
+/*
+ * arquivo.c
+ *
+ *  Created on: 28 de out de 2019
+ *      Author: carlos
+ */
+
+
+// codigo de implementação de arquivos em C
+
---
+++ ProjetoLP/src/arquivos.h
@@ -0,0 +1,13 @@
+/*
+ * arquivos.h
+ *
+ *  Created on: 28 de out de 2019
+ *      Author: carlos
+ */
+
+#ifndef ARQUIVOS_H_
+#define ARQUIVOS_H_
+
+
+
+#endif /* ARQUIVOS_H_ */
